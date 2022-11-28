40079bc5f68eea124dbdfb4fff302dddc05472f1
Fabián Abel Sajama <sajama230@gmail.com>
2022-11-28 19:39:33 -0300

    agregado de paquete models

---
+++ InmobiliariaApi/Modelos/Models/ClienteData.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modelos.Models
+{
+    public class ClienteData
+    {
+        [Key]
+        public int id_cliente { get; set; }
+
+        [StringLength(50)]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string NombreCliente { get; set; }
+        [StringLength(255)]
+        public string DireccionCliente { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [StringLength(50)]
+        public string CorreoCliente { get; set; }
+
+        [MaxLength(12)]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public int TelefonoCliente { get; set; }
+    }
+}
