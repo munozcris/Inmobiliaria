bebda4dfb15c575b5aeb1c4416e7d382aa6f7047
munozcris <cris091187965@gmail.com>
2022-12-04 22:32:34 -0300

    Entities

--- InmobiliariaApi/Entities/Inmueble.cs
+++ InmobiliariaApi/Entities/Inmueble.cs
@@ -1,6 +1,58 @@
-﻿namespace Entities
+﻿using Modelos.Models;
+
+namespace Entities
 {
     public class Inmueble
     {
+        private int id_inmueble;
+        private TipoInmuebleData tipo_inmueble_id;
+        private string desc_inmueble;
+        private string ubic_inmueble;
+        private decimal costo_inmueble;
+
+        public Inmueble(InmuebleData inmuebleData)
+        {
+            this.id_inmueble = inmuebleData.id_inmueble;
+            this.tipo_inmueble_id = inmuebleData.Tipo_Inmueble;
+            this.desc_inmueble = inmuebleData.desc_inmueble;
+            this.ubic_inmueble = inmuebleData.ubic_inmueble;
+            this.costo_inmueble = inmuebleData.costo_inmueble;
+        }
+
+        public int Id_Inmueble
+        {
+            get
+            {
+                return id_inmueble;
+            }
+            set
+            {
+                id_inmueble = value;
+            }
+        }
+
+        public string Desc_Inmueble
+        {
+            get { return desc_inmueble; }
+            set { desc_inmueble = value; }
+        }
+
+        public decimal Costo_Inmueble
+        {
+            get { return costo_inmueble; }
+            set { costo_inmueble = value; }
+        }
+
+        public string Ubic_Inmueble
+        {
+            get { return ubic_inmueble; }
+            set { ubic_inmueble = value; }
+        }
+
+        public TipoInmuebleData TipoInmueble
+        {
+            get { return tipo_inmueble_id; }
+            set { tipo_inmueble_id = value; }
+        }
     }
 }
--- InmobiliariaApi/Entities/TipoInmueble.cs
+++ InmobiliariaApi/Entities/TipoInmueble.cs
@@ -1,6 +1,28 @@
-﻿namespace Entities
+﻿using Modelos.Models;
+
+namespace Entities
 {
     public class TipoInmueble
     {
+        private int id_tipo_inmueble;
+        private string Desc_tipo_inmueble;
+
+        public TipoInmueble(TipoInmuebleData inmuebleData)
+        {
+            this.id_tipo_inmueble = inmuebleData.id_tipo_inmueble;
+            this.Desc_tipo_inmueble = inmuebleData.Desc_tipo_inmueble;
+        }
+
+        public int Id_Tipo_Inmueble
+        {
+            get { return id_tipo_inmueble; }
+            set { id_tipo_inmueble = value; }
+        }
+
+        public string Desc_Tipo_Inmueble
+        {
+            get { return Desc_tipo_inmueble; }
+            set { Desc_tipo_inmueble = value; }
+        }
     }
 }
